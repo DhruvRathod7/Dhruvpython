3d73b73e2dc92f5382e4358a8d1842edf2f83209
DhruvRathod77 <rathoddhruv77@gmail.com>
2022-01-09 00:34:56 +0530

    pdf

---
+++ dmpdf/dmpdf/tests/data/pdf2text.py
@@ -0,0 +1,17 @@
+import math
+
+class Converter:
+    """A simple Converter for converting PDFs to text."""
+
+    
+    def convert(self, Path):
+        """
+        Convert the given PDF to text.
+    
+        Parameters:
+        Path (str): The Path to a PDF file.
+        return:
+        str: The content of the PDF file as text.
+
+        """
+        print("pdf2text")
