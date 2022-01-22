cac0733732a682e4ad9865caddbd8fd2fbc7acad
DhruvRathod77 <rathoddhruv77@gmail.com>
2022-01-22 22:27:48 +0530

    Django project

--- Django/Dhruv/myApp/views.py
+++ Django/Dhruv/myApp/views.py
@@ -12,12 +12,12 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request, 'about.html')
 
 
 def services(request):
-    return HttpResponse("this is services page")
+    return render(request, 'services.html')
 
 
 def contact(request):
-    return HttpResponse("this is contact page")
+    return render(request, 'contact.html')
